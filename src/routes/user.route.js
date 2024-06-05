08eed75056dd1ea8e10f619279555475b19eeda6
akshay07k <akshaykumar5651t@gmail.com>
2024-06-05 22:05:35 +0530

    debugging

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registeUser } from "../controllers/user.controller";
+import { registeUser } from "../controllers/user.controller.js";
 import { upload } from '../middlewares/multer.middleware.js'
 
 const router = Router()
